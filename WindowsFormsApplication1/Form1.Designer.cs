0e4ccbf54742fe38612dac1eda3575c98c919b09
L. Pavljuk <pavljuk@seznam.cz>
2019-06-17 00:16:36 +0200

    First commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.dbgButt = new System.Windows.Forms.Button();
+            this.dbgProcNameBox = new System.Windows.Forms.TextBox();
+            this.startPauseButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stateLabel.Location = new System.Drawing.Point(277, 9);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(41, 30);
+            this.stateLabel.TabIndex = 0;
+            this.stateLabel.Text = "Test";
+            // 
+            // dbgButt
+            // 
+            this.dbgButt.Location = new System.Drawing.Point(205, 115);
+            this.dbgButt.Name = "dbgButt";
+            this.dbgButt.Size = new System.Drawing.Size(75, 23);
+            this.dbgButt.TabIndex = 1;
+            this.dbgButt.Text = "Debug";
+            this.dbgButt.UseVisualStyleBackColor = true;
+            this.dbgButt.Visible = false;
+            this.dbgButt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dbgProcNameBox
+            // 
+            this.dbgProcNameBox.Location = new System.Drawing.Point(286, 115);
+            this.dbgProcNameBox.Name = "dbgProcNameBox";
+            this.dbgProcNameBox.Size = new System.Drawing.Size(138, 22);
+            this.dbgProcNameBox.TabIndex = 2;
+            this.dbgProcNameBox.Visible = false;
+            // 
+            // startPauseButton
+            // 
+            this.startPauseButton.Location = new System.Drawing.Point(212, 43);
+            this.startPauseButton.Name = "startPauseButton";
+            this.startPauseButton.Size = new System.Drawing.Size(171, 38);
+            this.startPauseButton.TabIndex = 3;
+            this.startPauseButton.UseVisualStyleBackColor = true;
+            this.startPauseButton.Click += new System.EventHandler(this.startPauseButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 91);
+            this.Controls.Add(this.startPauseButton);
+            this.Controls.Add(this.dbgProcNameBox);
+            this.Controls.Add(this.dbgButt);
+            this.Controls.Add(this.stateLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pauser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Button dbgButt;
+        private System.Windows.Forms.TextBox dbgProcNameBox;
+        private System.Windows.Forms.Button startPauseButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        byte suspended = 2;
+        int g_id = 0;
+        string procname = "mysummercar";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scanForGameState(procname);
+            this.timer1.Enabled = true;
+        }
+
+        private void scanForGameState(string procname)
+        {
+            int id = -1;
+            Process[] processes = Process.GetProcessesByName(procname);
+            foreach (Process theprocess in processes)
+            {
+                if (theprocess.ProcessName == procname)
+                {
+                    id = theprocess.Id;
+                    break;
+                }
+            }
+            if (id != -1)
+            {
+                Process proc = Process.GetProcessById(id);
+                switch (proc.Threads[0].ThreadState)
+                {
+                    case ThreadState.Initialized:
+                    case ThreadState.Ready:
+                    case ThreadState.Running:
+                    case ThreadState.Standby:
+                        stateLabel.Text = "Game is running";
+                        startPauseButton.Text = "Pause the game";
+                        suspended = 0;
+                        g_id = proc.Id;
+                        break;
+                    case ThreadState.Terminated:
+                        stateLabel.Text = "Game appears to have just quit - Restart me please? I am confused...";
+                        startPauseButton.Visible = false;
+                        suspended = 3;
+                        break;
+                    case ThreadState.Unknown:
+                        stateLabel.Text = "Something is happening to the game's process... Uuuum... Help? Please?";
+                        MessageBox.Show("I just don't know what went wrong - Please restart the game and this app and try again? If nothing helps, report to Aldar with the following info: ThreadState=" + proc.Threads[0].ThreadState);
+                        startPauseButton.Visible = false;
+                        suspended = 3;
+                        break;
+                    case ThreadState.Wait:
+                        if (proc.Threads[0].WaitReason == ThreadWaitReason.Suspended)
+                        {
+                            stateLabel.Text = "Game is paused!";
+                            startPauseButton.Text = "Resume the Game";
+                            suspended = 1;
+                            g_id = proc.Id;
+                        }
+                        else
+                        {
+                            if (proc.Threads[0].WaitReason == ThreadWaitReason.UserRequest)
+                            {
+                                stateLabel.Text = "Game is running";
+                                startPauseButton.Text = "Pause the game";
+                                suspended = 0;
+                                g_id = proc.Id;
+                            }
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                stateLabel.Text = "Game does not appear to be running!";
+                startPauseButton.Text = "Rescan";
+                suspended = 2;
+            }
+            stateLabel.Left = (this.Width / 2) - (stateLabel.Width / 2);
+            startPauseButton.Left = this.Width / 2 - startPauseButton.Width / 2;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dbgProcNameBox.Text != "")
+            {
+                
+                int id=-1;
+                string procname = dbgProcNameBox.Text;               
+                Process[] all = Process.GetProcessesByName(procname);
+                foreach (Process theprocess in all)
+                {
+                    if (theprocess.ProcessName == procname)
+                    {
+                        id = theprocess.Id;
+                        break;
+                    }
+                }
+                
+                if (id >= 0)
+                {
+                    Process proc = Process.GetProcessById(id);
+                    if (proc.Threads[0].ThreadState == ThreadState.Wait)
+                    {
+                        if (proc.Threads[0].WaitReason == ThreadWaitReason.Suspended)
+                        {
+                            proc.Resume();
+                            proc.Refresh();
+                            if (proc.Threads[0].ThreadState == ThreadState.Wait)
+                            {
+                                if (proc.Threads[0].WaitReason == ThreadWaitReason.Suspended)
+                                {
+                                    stateLabel.Text= "Thread still suspended, click again or report to the dev please!";
+                                }
+                            }
+                            stateLabel.Text = "Game is running again! If not, report it to the dev please!";                            
+                        }
+                    }
+                                        
+                    stateLabel.Left = this.Width / 2 - stateLabel.Width / 2;
+                }
+                else
+                {
+                    stateLabel.Text = "Process not Found!";
+                }
+            }
+        }
+
+        private void startPauseButton_Click(object sender, EventArgs e)
+        {
+            int l_suspended = suspended;
+            switch (suspended)
+            {
+                case 0:
+                case 1:
+                    scanForGameState(procname);
+                    if (l_suspended == suspended) changeGameState();
+                    break;
+                case 2:
+                    scanForGameState(procname);
+                    break;
+                case 3:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        private void changeGameState()
+        {
+            int state_snapshot = suspended;
+            Process proc = Process.GetProcessById(g_id);
+            if (suspended == 0)
+            {
+                proc.Suspend();
+            }
+            else if (suspended == 1)
+            {
+                proc.Resume();
+            }
+            proc.Refresh();
+            scanForGameState(procname);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            scanForGameState(procname);
+        }
+    }
+}
